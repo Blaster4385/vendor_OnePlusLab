6b122c3994bfcf6d8ff12eb3a8d4f428a446cf15
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-11-16 16:44:00 +0530

    OnePlusLab: lemonkebab > opkona

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE), kebab lemonades lemonkebab))
+ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE), kebab lemonades opkona))
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
